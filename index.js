53049250f0b0ddcc505cc228a27c17cb13e4a553
Mohammad Robiul Hossen <robim6225@gmail.com>
2026-01-08 16:25:40 +0600

    solved PingPong Challenge

--- index.js
+++ index.js
@@ -118,4 +118,20 @@ const factorial = (num) => {
   return result;
 };
 const res9 = factorial(5);
-console.log(res9);
+// console.log(res9);
+
+// Problem-10 ==> PingPong Challenge
+const pingPong = () => {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("pingPong");
+    } else if (i % 3 === 0) {
+      console.log("ping");
+    } else if (i % 5 === 0) {
+      console.log("pong");
+    } else {
+      console.log(i);
+    }
+  }
+};
+// pingPong();
